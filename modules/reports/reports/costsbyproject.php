b2b1c87926fef3a938b753f08d2de9cbe8b2a3e0
caseysoftware <keith@blueparabola.com>
2011-01-02 01:53:57 -0600

    implementing the last of the reports;

--- modules/reports/reports/costsbyproject.php
+++ modules/reports/reports/costsbyproject.php
@@ -2,8 +2,13 @@
 if (!defined('W2P_BASE_DIR')) {
 	die('You should not access this file directly.');
 }
+
 global $AppUI, $cal_sdf;
 $AppUI->loadCalendarJS();
+
+$company_id = (int) w2PgetParam($_POST, 'company_id', 0);
+$active_projects = w2PgetParam($_POST, 'active_projects', 0);
+$active_projects = (isset($_POST['company_id'])) ? $active_projects : 1;
 
 /**
  * Generates a report of the task logs for given dates
@@ -21,28 +26,14 @@ if (!$log_start_date) {
 	$start_date->subtractSpan(new Date_Span('14,0,0,0'));
 }
 $end_date->setTime(23, 59, 59);
+
+$company = new CCompany();
+$companies = $company->getAllowedRecords($AppUI->user_id, 'company_id,company_name', 'company_name');
+$companies = arrayMerge(array('0' => ''), $companies);
 ?>
-<script language="javascript">
-function setDate( frm_name, f_date ) {
-	fld_date = eval( 'document.' + frm_name + '.' + f_date );
-	fld_real_date = eval( 'document.' + frm_name + '.' + 'log_' + f_date );
-	if (fld_date.value.length>0) {
-      if ((parseDate(fld_date.value))==null) {
-            alert('The Date/Time you typed does not match your prefered format, please retype.');
-            fld_real_date.value = '';
-            fld_date.style.backgroundColor = 'red';
-        } else {
-        	fld_real_date.value = formatDate(parseDate(fld_date.value), 'yyyyMMdd');
-        	fld_date.value = formatDate(parseDate(fld_date.value), '<?php echo $cal_sdf ?>');
-            fld_date.style.backgroundColor = '';
-  		}
-	} else {
-      	fld_real_date.value = '';
-	}
-}
-</script>
+
 <form name="editFrm" action="index.php?m=reports" method="post" accept-charset="utf-8">
-    <input type="hidden" name="project_id" value="<?php echo $project_id; ?>" />
+    <input type="hidden" name="company_id" value="<?php echo $company_id; ?>" />
     <input type="hidden" name="report_type" value="<?php echo $report_type; ?>" />
     <?php
     if (function_exists('styleRenderBoxTop')) {
@@ -51,6 +42,10 @@ function setDate( frm_name, f_date ) {
     ?>
     <table cellspacing="0" cellpadding="4" border="0" width="100%" class="std">
         <tr>
+            <td align="right" nowrap="nowrap"><?php echo $AppUI->_('For Company'); ?>:</td>
+            <td nowrap="nowrap">
+                <?php echo arraySelect($companies, 'company_id', 'class="text" size="1"', $company_id); ?> *
+            </td>
             <td align="right" nowrap="nowrap"><?php echo $AppUI->_('For period'); ?>:</td>
             <td nowrap="nowrap">
                 <input type="hidden" name="log_start_date" id="log_start_date" value="<?php echo $start_date ? $start_date->format(FMT_TIMESTAMP_DATE) : ''; ?>" />
@@ -72,6 +67,11 @@ function setDate( frm_name, f_date ) {
             echo 'checked="checked"' ?> />
                 <label for="log_pdf"><?php echo $AppUI->_('Make PDF'); ?></label>
             </td>
+            <td nowrap="nowrap">
+                <input type="checkbox" name="active_projects" id="active_projects" <?php if ($active_projects)
+            echo 'checked="checked"' ?> />
+                <label for="active_projects"><?php echo $AppUI->_('Active Projects Only'); ?></label>
+            </td>
             <td align="right" width="50%" nowrap="nowrap">
                 <input class="button" type="submit" name="do_report" value="<?php echo $AppUI->_('submit'); ?>" />
             </td>
@@ -79,10 +79,10 @@ function setDate( frm_name, f_date ) {
     </table>
 </form>
 
-<table width="100%" class="tbl" cellspacing="1" cellpadding="2" border="0">
+<table width="100%" class="tbl" cellspacing="1" cellpadding="3" border="0">
 	<tr>
         <th width="10px" nowrap="true">Work</th>
-        <th>Task Name</th>
+        <th>Project Name</th>
         <th width="10px" align="center">Task Owner</th>
         <th width="10px" align="center">Start Date</th>
         <th width="10px" align="center">Finish Date</th>
@@ -97,30 +97,35 @@ function setDate( frm_name, f_date ) {
     </tr>
     <?php
     //TODO: rotate the headers by 90 degrees?
-    $task = new CTask();
-    $taskList = $task->getAllowedTaskList($AppUI, $project_id);
+    $activeOnly = ($active_projects) ? true : false;
+    $projectList = CCompany::getProjects($AppUI, $company_id, $activeOnly);
     $bcode = new bcode();
+    $project = new CProject();
 
-    if (count($taskList)) {
-        foreach ($taskList as $taskItem) {
-            $task->load($taskItem['task_id']);
-            $costs = $bcode->calculateTaskCost($taskItem['task_id'], $start_date, $end_date);
-            $tstart = new CDate($task->task_start_date);
-            $tend   = new CDate($task->task_end_date);
-            $workingDays = $tstart->workingDaysInSpan($tend);
+    if (count($projectList)) {
+        foreach ($projectList as $projectItem) {
+            $project->load($projectItem['project_id']);
+            $criticalTasks = $project->getCriticalTasks($projectItem['project_id']);
+
+            $costs = $bcode->calculateProjectCost($projectItem['project_id'], $start_date, $end_date);
+            $pstart = new CDate($project->project_start_date);
+            $pend = intval($criticalTasks[0]['task_end_date']) ? new CDate($criticalTasks[0]['task_end_date']) : new CDate();
+            $workingDays = $pstart->workingDaysInSpan($pend);
             ?><tr>
-                <td><?php echo $task->task_percent_complete; ?>%</td>
-                <td>
-                    <a href="?m=tasks&amp;a=view&amp;task_id=<?php echo $task->task_id; ?>"><?php echo $task->task_name; ?></a>
+                <td width="10" align="right" style="border: outset #eeeeee 1px;background-color:#<?php echo $project->project_color_identifier; ?>">
+                    <font color="<?php echo bestColor($project->project_color_identifier); ?>"><?php echo sprintf('%.1f%%', $project->project_percent_complete); ?></font>
                 </td>
-                <td align="center"><?php echo CContact::getContactByUserid($task->task_owner); ?></td>
-                <td><?php echo $AppUI->formatTZAwareTime($task->task_start_date, $df); ?></td>
-                <td><?php echo $AppUI->formatTZAwareTime($task->task_end_date, $df); ?></td>
-                <td align="center"><?php echo (int) $task->task_target_budget; ?></td>
+                <td>
+                    <a href="?m=projects&amp;a=view&amp;project_id=<?php echo $project->project_id; ?>"><?php echo $project->project_name; ?></a>
+                </td>
+                <td align="center"><?php echo CContact::getContactByUserid($project->project_owner); ?></td>
+                <td><?php echo $AppUI->formatTZAwareTime($project->project_start_date, $df); ?></td>
+                <td><?php echo $AppUI->formatTZAwareTime($criticalTasks[0]['task_end_date'], $df); ?></td>
+                <td align="center"><?php echo (int) $project->project_target_budget; ?></td>
                 <td align="center"><?php echo (int) $costs['actualCost']; ?></td>
                 <td align="center">
                     <?php
-                    $diff = (int) ($task->task_target_budget - $costs['actualCost']);
+                    $diff = (int) ($project->project_target_budget - $costs['actualCost']);
                     echo ($diff < 0) ? '<span style="color: red;">' : '';
                     echo $diff;
                     echo ($diff < 0) ? '</span>' : '';
@@ -129,8 +134,8 @@ function setDate( frm_name, f_date ) {
                 <td align="center">
                     <?php
                     $perDiff = '-';
-                    if ($task->task_target_budget > 0) {
-                        $perDiff = 100 * $costs['actualCost'] / $task->task_target_budget;
+                    if ($project->project_target_budget > 0) {
+                        $perDiff = 100 * $costs['actualCost'] / $project->project_target_budget;
                         $perDiff = (int) $perDiff.'%';
                     }
                     echo $perDiff;
@@ -140,7 +145,7 @@ function setDate( frm_name, f_date ) {
                     <?php
                     $dailyBudget = '-';
                     if ($workingDays > 0) {
-                        $dailyBudget = (int) ($task->task_target_budget/$workingDays);
+                        $dailyBudget = (int) ($project->project_target_budget/$workingDays);
                     }
                     echo $dailyBudget;
                     ?>
@@ -175,7 +180,7 @@ function setDate( frm_name, f_date ) {
             </tr><?php
         }
     } else {
-        echo '<tr><td colspan="13">'.$AppUI->_('There are no tasks on this project').'</td></tr>';
+        echo '<tr><td colspan="13">'.$AppUI->_('There are no projects in this company').'</td></tr>';
     }
     ?>
 </table>
