dbd77f45c049f6bb6c6df1b04cbd7a891b03da7e
caseysoftware <keith@blueparabola.com>
2011-01-17 19:57:00 -0600

    wired in the budget tables; setup budget processing in do_project_aed

--- install/sql/mysql/029_budget_stuff.sql
+++ install/sql/mysql/029_budget_stuff.sql
@@ -8,3 +8,14 @@ INSERT INTO sysvals (sysval_key_id, sysval_title, sysval_value, sysval_value_id)
     (1, 'BudgetCategory', 'Licenses', 'licenses'),
     (1, 'BudgetCategory', 'Permits', 'permits'),
     (1, 'BudgetCategory', 'Travel', 'travel');
+
+CREATE TABLE `project_budgets` (
+    `budget_id` INT( 10 ) NOT NULL AUTO_INCREMENT ,
+    `budget_project` INT( 10 ) NOT NULL ,
+    `budget_task` INT( 10 ) NOT NULL ,
+    `budget_category` VARCHAR( 50 ) NOT NULL DEFAULT '',
+    `budget_amount` DECIMAL( 10, 2 ) NOT NULL ,
+    PRIMARY KEY ( `budget_id` ) ,
+    INDEX ( `budget_project` ),
+    INDEX ( `budget_task` )
+) ENGINE = InnoDB;
