30c32f04eb6c90793078692eb92580279f1b8a08
caseysoftware <keith@blueparabola.com>
2011-02-07 23:21:50 -0600

    applying a fix for contacts

---
+++ install/sql/mysql/030_contact_tweaks.sql
@@ -0,0 +1,6 @@
+
+ALTER TABLE `contacts` CHANGE `contact_email` `contact_email` VARCHAR( 255 )
+    CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
+
+ALTER TABLE `contacts` CHANGE `contact_phone` `contact_phone` VARCHAR( 30 )
+    CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
